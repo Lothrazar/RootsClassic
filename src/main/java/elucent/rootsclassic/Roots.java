2ef6822ddb0dcd07f02af9ecccb9b4311bb2bb4c
sam <samson.bassett@gmail.com>
2018-06-11 16:31:24 -0700

    gradle properties added

--- src/main/java/elucent/rootsclassic/Roots.java
+++ src/main/java/elucent/rootsclassic/Roots.java
@@ -15,16 +15,14 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-@Mod(modid = Const.MODID, name = Roots.NAME, version = Roots.VERSION)
+@Mod(modid = Const.MODID, useMetadata = true)
 public class Roots
 {
-	public static final String NAME = "Roots";
-    public static final String VERSION = "0.107";
-    
+
   public static CreativeTabs tab = new CreativeTabs(Const.MODID) {
     	@Override
     	public String getTabLabel(){
-    		return "roots";
+      return Const.MODID;
     	}
 		@Override
 		@SideOnly(Side.CLIENT)
