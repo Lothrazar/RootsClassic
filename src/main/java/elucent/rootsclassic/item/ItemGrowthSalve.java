4a9934ad3f10d10884c7a20f3e3f023e89120137
sam <samson.bassett@gmail.com>
2019-01-15 18:19:56 -0800

    1.1.3 #23 growth overhaul

--- src/main/java/elucent/rootsclassic/item/ItemGrowthSalve.java
+++ src/main/java/elucent/rootsclassic/item/ItemGrowthSalve.java
@@ -1,13 +1,13 @@
 package elucent.rootsclassic.item;
 
 import elucent.rootsclassic.Roots;
-import elucent.rootsclassic.Util;
+import net.minecraft.block.IGrowable;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
@@ -20,32 +20,49 @@ public class ItemGrowthSalve extends Item {
   }
 
   @Override
-  public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
-    ItemStack stack = player.getHeldItem(hand);
+  public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+    spawnGrowthParticle(world, player);
+    boolean anySuccess = applyGrowthHere(world, pos);
+    if (anySuccess && !player.capabilities.isCreativeMode) {
+      player.getHeldItem(hand).shrink(1);
+    }
+    return EnumActionResult.PASS;
+  }
+
+  public static boolean applyGrowthHere(World world, BlockPos pos) {
+    IBlockState iblockstate = world.getBlockState(pos);
+    if (iblockstate.getBlock() == Blocks.DIRT) {
+      world.setBlockState(pos, Blocks.GRASS.getDefaultState());
+      return true;
+    }
+    else if (iblockstate.getBlock() == Blocks.WATER &&
+        world.isAirBlock(pos.up())) {
+      world.setBlockState(pos.up(), Blocks.WATERLILY.getDefaultState());
+    }
+    else {
+      if (iblockstate.getBlock() instanceof IGrowable) {
+        IGrowable igrowable = (IGrowable) iblockstate.getBlock();
+        if (igrowable.canGrow(world, pos, iblockstate, world.isRemote)) {
+          if (!world.isRemote) {
+            if (igrowable.canUseBonemeal(world, world.rand, pos, iblockstate)) {
+              igrowable.grow(world, world.rand, pos, iblockstate);
+            }
+          }
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+
+  public static void spawnGrowthParticle(World world, EntityPlayer player) {
+    Vec3d lookVec = player.getLookVec();
     for (int i = 0; i < 40; i++) {
-      Vec3d lookVec = player.getLookVec();
       double velX = (lookVec.x * 0.75) + 0.5 * (itemRand.nextDouble() - 0.5);
       double velY = (lookVec.y * 0.75) + 0.5 * (itemRand.nextDouble() - 0.5);
       double velZ = (lookVec.z * 0.75) + 0.5 * (itemRand.nextDouble() - 0.5);
       Roots.proxy.spawnParticleMagicFX(world, player.posX + 0.5 * lookVec.x, player.posY + 1.5 + 0.5 * lookVec.y, player.posZ + 0.5 * lookVec.z, velX, velY, velZ, 39, 232, 55);
     }
-    BlockPos pos = Util.getRayTrace(world, player, 4);
-    for (int i = -2; i < 3; i++) {
-      for (int j = -2; j < 3; j++) {
-        BlockPos temp = pos.add(i, 0, j);
-        if (world.getBlockState(temp).getBlock() == Blocks.DIRT) {
-          if (i == 0 && j == 0) {
-            world.setBlockState(temp, Blocks.GRASS.getDefaultState());
-          }
-          else if (itemRand.nextInt(Math.abs(i) + Math.abs(j)) == 0) {
-            world.setBlockState(temp, Blocks.GRASS.getDefaultState());
-          }
-        }
-      }
-    }
-    if (!player.capabilities.isCreativeMode) {
-      stack.shrink(1);
-    }
-    return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, stack);
   }
 }
--- src/main/java/elucent/rootsclassic/tileentity/TileEntityStandingStoneGrower.java
+++ src/main/java/elucent/rootsclassic/tileentity/TileEntityStandingStoneGrower.java
@@ -1,17 +1,21 @@
 package elucent.rootsclassic.tileentity;
 
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import elucent.rootsclassic.Roots;
-import net.minecraft.block.BlockCrops;
+import elucent.rootsclassic.item.ItemGrowthSalve;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.math.BlockPos;
 
 public class TileEntityStandingStoneGrower extends TEBase implements ITickable {
 
-  private static final int RADIUS = 10;
+  private static final double PCT_CHANCE_PER_BLOCK = 0.03;
+  private static final int TICK_SPEED = 100;
+  private static final int MAX_TICKER = 500 * 20;
+  private static final int RADIUS = 4;
   int ticker = 0;
-  Random random = new Random();
 
   public TileEntityStandingStoneGrower() {
     super();
@@ -30,29 +34,44 @@ public class TileEntityStandingStoneGrower extends TEBase implements ITickable {
 
   @Override
   public void update() {
+    updateTicker();
+    spawnParticles();
+    if (ticker % TICK_SPEED == 0) {
+      applyGrowthToArea();
+    }
+  }
+
+  private void updateTicker() {
     ticker++;
+    if (ticker >= MAX_TICKER) {
+      ticker = 0;
+    }
+  }
+
+  private void applyGrowthToArea() {
+    List<BlockPos> positions = new ArrayList<BlockPos>();
+    for (int xp = -1 * RADIUS; xp <= RADIUS; xp++) {
+      for (int zp = -1 * RADIUS; zp <= RADIUS; zp++) {
+        positions.add(this.getPos().add(xp, -1, zp));
+        positions.add(this.getPos().add(xp, -2, zp));
+      }
+    }
+    Collections.shuffle(positions);
+    for (BlockPos pos : positions) {
+      if (world.rand.nextDouble() < PCT_CHANCE_PER_BLOCK) {
+        //this percent chance to actually fire
+        ItemGrowthSalve.applyGrowthHere(world, pos);
+      }
+    }
+  }
+
+  private void spawnParticles() {
     if (ticker % 5 == 0) {
       for (double i = 0; i < 720; i += 45.0) {
         double xShift = 0.5 * Math.sin(Math.PI * (i / 360.0));
         double zShift = 0.5 * Math.cos(Math.PI * (i / 360.0));
         Roots.proxy.spawnParticleMagicAuraFX(this.getWorld(), this.getPos().getX() + 0.5 + xShift, this.getPos().getY() + 0.5, this.getPos().getZ() + 0.5 + zShift, 0, 0, 0, 32, 255, 32);
       }
-    }
-    if (ticker % 100 == 0) {
-      for (int xp = RADIUS; xp <= RADIUS; xp++) {
-        for (int zp = RADIUS; zp <= RADIUS; zp++) {
-          BlockPos pos = this.getPos().add(xp, -1, zp);
-          if (this.getWorld().getBlockState(pos).getBlock() instanceof BlockCrops && random.nextInt(40) == 0) {
-            int age = this.getWorld().getBlockState(pos).getBlock().getMetaFromState(this.getWorld().getBlockState(pos));
-            if (age < ((BlockCrops) this.getWorld().getBlockState(pos).getBlock()).getMaxAge()) {
-              this.getWorld().setBlockState(pos, ((BlockCrops) this.getWorld().getBlockState(pos).getBlock()).withAge(age + 1));
-            }
-          }
-        }
-      }
-    }
-    if (ticker >= 2000) {
-      ticker = 0;
     }
   }
 }
