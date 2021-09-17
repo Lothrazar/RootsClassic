c297a182b323691ac355722767c72cf56f56338d
Mrbysco <Mrbysco@users.noreply.github.com>
2021-09-17 16:12:28 +0200

    Fix the altar shape allowing xray

--- src/main/java/elucent/rootsclassic/block/altar/AltarBlock.java
+++ src/main/java/elucent/rootsclassic/block/altar/AltarBlock.java
@@ -5,14 +5,24 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.IBooleanFunction;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 import elucent.rootsclassic.block.BaseTEBlock;
 
-public class AltarBlock extends BaseTEBlock {
+import java.util.stream.Stream;
 
-  private static final VoxelShape SHAPE = Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 12.0D, 16.0D);
+public class AltarBlock extends BaseTEBlock {
+  private static final VoxelShape SHAPE = Stream.of(
+          Block.makeCuboidShape(0, 8, 0, 16, 12, 16),
+          Block.makeCuboidShape(4, 0, 4, 12, 8, 12),
+          Block.makeCuboidShape(2, 4, 2, 6, 8, 6),
+          Block.makeCuboidShape(10, 4, 2, 14, 8, 6),
+          Block.makeCuboidShape(10, 4, 10, 14, 8, 14),
+          Block.makeCuboidShape(2, 4, 10, 6, 8, 14)
+  ).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR)).get();
 
   public AltarBlock(Properties properties) {
     super(properties);
