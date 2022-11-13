a2cfc225f61bc396fb87151457072e0e5af690c4
PssbleTrngle <nik@somethingcatchy.net>
2022-11-13 17:11:41 +0100

    missed a few imports

--- src/main/java/elucent/rootsclassic/recipe/RitualRecipe.java
+++ src/main/java/elucent/rootsclassic/recipe/RitualRecipe.java
@@ -22,6 +22,7 @@ import net.minecraft.world.Container;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
