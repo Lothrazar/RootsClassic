c09318ed2512422d6a22df4cd3059825344ac9b9
lothrazar <samson.bassett@gmail.com>
2023-07-12 14:56:02 -0700

    nice lots of unused functions to delete nom nom nom

--- src/main/java/elucent/rootsclassic/util/RootsUtil.java
+++ src/main/java/elucent/rootsclassic/util/RootsUtil.java
@@ -1,9 +1,11 @@
 package elucent.rootsclassic.util;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import elucent.rootsclassic.Const;
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.Container;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
@@ -12,296 +14,84 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.phys.AABB;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
 public class RootsUtil {
-	private static final Random random = new Random();
 
-	public static double randomDouble(double min, double max) {
-		double range = max - min;
-		double scale = random.nextDouble() * range;
-		return scale + min;
-	}
+  private static final Random random = new Random();
 
-	public static BlockPos getRayTrace(Level levelAccessor, LivingEntity livingEntity, int reachDistance) {
-		double x = livingEntity.getX();
-		double y = livingEntity.getY() + livingEntity.getEyeHeight();
-		double z = livingEntity.getZ();
-		for (int i = 0; i < reachDistance * 10.0; i++) {
-			x += livingEntity.getLookAngle().x * 0.1;
-			y += livingEntity.getLookAngle().y * 0.1;
-			z += livingEntity.getLookAngle().z * 0.1;
-			if (levelAccessor.getBlockState(BlockPos.containing(x, y, z)).getBlock() != Blocks.AIR) {
-				return BlockPos.containing(x, y, z);
-			}
-		}
-		return BlockPos.containing(x, y, z);
-	}
+  public static double randomDouble(double min, double max) {
+    double range = max - min;
+    double scale = random.nextDouble() * range;
+    return scale + min;
+  }
 
-	public static void addTickTracking(Entity entity) {
-		if (entity.getPersistentData().contains(Const.NBT_TRACK_TICKS)) {
-			entity.getPersistentData().putInt(Const.NBT_TRACK_TICKS, entity.getPersistentData().getInt(Const.NBT_TRACK_TICKS) + 1);
-		} else {
-			entity.getPersistentData().putInt(Const.NBT_TRACK_TICKS, 1);
-		}
-	}
+  public static BlockPos getRayTrace(Level levelAccessor, LivingEntity livingEntity, int reachDistance) {
+    double x = livingEntity.getX();
+    double y = livingEntity.getY() + livingEntity.getEyeHeight();
+    double z = livingEntity.getZ();
+    for (int i = 0; i < reachDistance * 10.0; i++) {
+      x += livingEntity.getLookAngle().x * 0.1;
+      y += livingEntity.getLookAngle().y * 0.1;
+      z += livingEntity.getLookAngle().z * 0.1;
+      if (levelAccessor.getBlockState(BlockPos.containing(x, y, z)).getBlock() != Blocks.AIR) {
+        return BlockPos.containing(x, y, z);
+      }
+    }
+    return BlockPos.containing(x, y, z);
+  }
 
-	public static void decrementTickTracking(Entity entity) {
-		if (entity.getPersistentData().contains(Const.NBT_TRACK_TICKS)) {
-			entity.getPersistentData().putInt(Const.NBT_TRACK_TICKS, entity.getPersistentData().getInt(Const.NBT_TRACK_TICKS) - 1);
-			if (entity.getPersistentData().getInt(Const.NBT_TRACK_TICKS) == 0) {
-				entity.removeTag(Const.NBT_TRACK_TICKS);
-			}
-		}
-	}
+  public static void addTickTracking(Entity entity) {
+    if (entity.getPersistentData().contains(Const.NBT_TRACK_TICKS)) {
+      entity.getPersistentData().putInt(Const.NBT_TRACK_TICKS, entity.getPersistentData().getInt(Const.NBT_TRACK_TICKS) + 1);
+    }
+    else {
+      entity.getPersistentData().putInt(Const.NBT_TRACK_TICKS, 1);
+    }
+  }
 
-	public static Entity getRayTraceEntity(Level levelAccessor, LivingEntity livingEntity, int reachDistance) {
-		double x = livingEntity.getX();
-		double y = livingEntity.getY() + livingEntity.getEyeHeight();
-		double z = livingEntity.getZ();
-		for (int i = 0; i < reachDistance * 10.0; i++) {
-			x += livingEntity.getLookAngle().x * 0.1;
-			y += livingEntity.getLookAngle().y * 0.1;
-			z += livingEntity.getLookAngle().z * 0.1;
-			List<Entity> entities = levelAccessor.getEntitiesOfClass(Entity.class, new AABB(x - 0.1, y - 0.1, z - 0.1, x + 0.1, y + 0.1, z + 0.1));
-			if (entities.size() > 0) {
-				if (entities.get(0).getUUID() != livingEntity.getUUID()) {
-					return entities.get(0);
-				}
-			}
-		}
-		return null;
-	}
+  public static void decrementTickTracking(Entity entity) {
+    if (entity.getPersistentData().contains(Const.NBT_TRACK_TICKS)) {
+      entity.getPersistentData().putInt(Const.NBT_TRACK_TICKS, entity.getPersistentData().getInt(Const.NBT_TRACK_TICKS) - 1);
+      if (entity.getPersistentData().getInt(Const.NBT_TRACK_TICKS) == 0) {
+        entity.removeTag(Const.NBT_TRACK_TICKS);
+      }
+    }
+  }
 
-	public static int intColor(int r, int g, int b) {
-		return (r * 65536 + g * 256 + b);
-	}
+  public static Entity getRayTraceEntity(Level levelAccessor, LivingEntity livingEntity, int reachDistance) {
+    double x = livingEntity.getX();
+    double y = livingEntity.getY() + livingEntity.getEyeHeight();
+    double z = livingEntity.getZ();
+    for (int i = 0; i < reachDistance * 10.0; i++) {
+      x += livingEntity.getLookAngle().x * 0.1;
+      y += livingEntity.getLookAngle().y * 0.1;
+      z += livingEntity.getLookAngle().z * 0.1;
+      List<Entity> entities = levelAccessor.getEntitiesOfClass(Entity.class, new AABB(x - 0.1, y - 0.1, z - 0.1, x + 0.1, y + 0.1, z + 0.1));
+      if (entities.size() > 0) {
+        if (entities.get(0).getUUID() != livingEntity.getUUID()) {
+          return entities.get(0);
+        }
+      }
+    }
+    return null;
+  }
 
-	public static boolean itemListMatchInventoryWithSize(Container i1, List<ItemStack> i2) {
-		List<ItemStack> recipe = new ArrayList<>();
-		for (int i = 0; i < i1.getContainerSize(); i++) {
-			recipe.add(i1.getItem(i));
-		}
-		List<ItemStack> available = new ArrayList<>(i2);
-		for (int i = 0; i < recipe.size(); i++) {
-			if (recipe.get(i).isEmpty()) {
-				recipe.remove(i);
-				i--;
-			}
-		}
-		for (int i = 0; i < available.size(); i++) {
-			if (available.get(i).isEmpty()) {
-				available.remove(i);
-				i--;
-			}
-		}
-		if (available.size() == recipe.size()) {
-			for (ItemStack itemStack : available) {
-				boolean endIteration = false;
-				for (int i = 0; i < recipe.size() && !endIteration; i++) {
-					if (ItemStack.isSameItem(itemStack, recipe.get(i))) {
-						recipe.remove(i);
-						endIteration = true;
-					}
-				}
-			}
-		}
-		return recipe.size() == 0;
-	}
+  public static int intColor(int r, int g, int b) {
+    return (r * 65536 + g * 256 + b);
+  }
 
-	public static boolean itemListMatchInventory(Container i1, List<ItemStack> i2) {
-		List<ItemStack> recipe = new ArrayList<>();
-		for (int i = 0; i < i1.getContainerSize(); i++) {
-			recipe.add(i1.getItem(i));
-		}
-		List<ItemStack> available = new ArrayList<>(i2);
-		for (int i = 0; i < recipe.size(); i++) {
-			if (recipe.get(i).isEmpty()) {
-				recipe.remove(i);
-				i--;
-			}
-		}
-		for (int i = 0; i < available.size(); i++) {
-			if (available.get(i).isEmpty()) {
-				available.remove(i);
-				i--;
-			}
-		}
-		if (available.size() >= recipe.size()) {
-			for (ItemStack itemStack : available) {
-				boolean endIteration = false;
-				for (int i = 0; i < recipe.size() && !endIteration; i++) {
-					if (ItemStack.isSameItem(itemStack, recipe.get(i))) {
-						recipe.remove(i);
-						endIteration = true;
-					}
-				}
-			}
-		}
-		return recipe.size() == 0;
-	}
+  public static void randomlyRepair(RandomSource rnd, ItemStack stack) {
+    if (stack.isDamaged() && rnd.nextInt(80) == 0) {
+      stack.setDamageValue(stack.getDamageValue() - 1);
+    }
+  }
 
-	public static boolean itemListMatchesIngredients(List<Ingredient> i1, List<ItemStack> i2) {
-		List<Ingredient> recipe = new ArrayList<>(i1);
-		List<ItemStack> available = new ArrayList<>(i2);
-		for (int i = 0; i < recipe.size(); i++) {
-			if (recipe.get(i).isEmpty()) {
-				recipe.remove(i);
-				i--;
-			}
-		}
-		for (int i = 0; i < available.size(); i++) {
-			if (available.get(i).isEmpty()) {
-				available.remove(i);
-				i--;
-			}
-		}
-		if (available.size() >= recipe.size()) {
-			for (ItemStack itemStack : available) {
-				boolean endIteration = false;
-				for (int i = 0; i < recipe.size() && !endIteration; i++) {
-					if (recipe.get(i).test(itemStack)) {
-						recipe.remove(i);
-						endIteration = true;
-					}
-				}
-			}
-		}
-		return recipe.size() == 0;
-	}
-
-	public static boolean itemListMatchesIngredientsWithSize(List<Ingredient> i1, List<ItemStack> i2) {
-		List<Ingredient> recipe = new ArrayList<>(i1);
-		List<ItemStack> available = new ArrayList<>(i2);
-		for (int i = 0; i < recipe.size(); i++) {
-			if (recipe.get(i).isEmpty()) {
-				recipe.remove(i);
-				i--;
-			}
-		}
-		for (int i = 0; i < available.size(); i++) {
-			if (available.get(i).isEmpty()) {
-				available.remove(i);
-				i--;
-			}
-		}
-		if (available.size() == recipe.size()) {
-			for (ItemStack itemStack : available) {
-				boolean endIteration = false;
-				for (int i = 0; i < recipe.size() && !endIteration; i++) {
-					if (recipe.get(i).test(itemStack)) {
-						recipe.remove(i);
-						endIteration = true;
-					}
-				}
-			}
-		}
-		return recipe.size() == 0;
-	}
-
-	public static boolean itemListsMatchWithSize(List<ItemStack> i1, List<ItemStack> i2) {
-		List<ItemStack> recipe = new ArrayList<>(i1);
-		List<ItemStack> available = new ArrayList<>(i2);
-		for (int i = 0; i < recipe.size(); i++) {
-			if (recipe.get(i).isEmpty()) {
-				recipe.remove(i);
-				i--;
-			}
-		}
-		for (int i = 0; i < available.size(); i++) {
-			if (available.get(i).isEmpty()) {
-				available.remove(i);
-				i--;
-			}
-		}
-		if (available.size() == recipe.size()) {
-			for (ItemStack itemStack : available) {
-				boolean endIteration = false;
-				for (int i = 0; i < recipe.size() && !endIteration; i++) {
-					if (ItemStack.isSameItem(itemStack, recipe.get(i))) {
-						recipe.remove(i);
-						endIteration = true;
-					}
-				}
-			}
-		}
-		return recipe.size() == 0;
-	}
-
-	public static boolean itemListsMatch(List<ItemStack> i1, List<ItemStack> i2) {
-		List<ItemStack> recipe = new ArrayList<>(i1);
-		List<ItemStack> available = new ArrayList<>(i2);
-		for (int i = 0; i < recipe.size(); i++) {
-			if (recipe.get(i).isEmpty()) {
-				recipe.remove(i);
-				i--;
-			}
-		}
-		for (int i = 0; i < available.size(); i++) {
-			if (available.get(i).isEmpty()) {
-				available.remove(i);
-				i--;
-			}
-		}
-		if (available.size() >= recipe.size()) {
-			for (ItemStack itemStack : available) {
-				boolean endIteration = false;
-				for (int i = 0; i < recipe.size() && !endIteration; i++) {
-					if (ItemStack.isSameItem(itemStack, recipe.get(i))) {
-						recipe.remove(i);
-						endIteration = true;
-					}
-				}
-			}
-		}
-		return recipe.size() == 0;
-	}
-
-	public static boolean ingredientListMatch(List<Ingredient> i1, List<Ingredient> i2) {
-		List<Ingredient> recipe = new ArrayList<>(i1);
-		List<Ingredient> available = new ArrayList<>(i2);
-		for (int i = 0; i < recipe.size(); i++) {
-			if (recipe.get(i).isEmpty()) {
-				recipe.remove(i);
-				i--;
-			}
-		}
-		for (int i = 0; i < available.size(); i++) {
-			if (available.get(i).isEmpty()) {
-				available.remove(i);
-				i--;
-			}
-		}
-		if (available.size() >= recipe.size()) {
-			for (Ingredient ingredient : available) {
-				boolean endIteration = false;
-				for (int i = 0; i < recipe.size() && !endIteration; i++) {
-					if (ItemStack.isSameItem(ingredient.getItems()[0], recipe.get(i).getItems()[0])) {
-						recipe.remove(i);
-						endIteration = true;
-					}
-				}
-			}
-		}
-		return recipe.size() == 0;
-	}
-
-	public static void randomlyRepair(RandomSource rnd, ItemStack stack) {
-		if (stack.isDamaged() && rnd.nextInt(80) == 0) {
-			stack.setDamageValue(stack.getDamageValue() - 1);
-		}
-	}
-
-	public static boolean matchesIngredients(List<ItemStack> inv, List<Ingredient> ingredients) {
-		if (inv.size() != ingredients.size()) return false;
-
-		var available = new ArrayList<>(inv);
-		return ingredients.stream().allMatch(ingredient -> {
-			var match = available.stream().filter(ingredient).findFirst();
-			match.ifPresent(available::remove);
-			return match.isPresent();
-		});
-	}
+  public static boolean matchesIngredients(List<ItemStack> inv, List<Ingredient> ingredients) {
+    if (inv.size() != ingredients.size()) return false;
+    var available = new ArrayList<>(inv);
+    return ingredients.stream().allMatch(ingredient -> {
+      var match = available.stream().filter(ingredient).findFirst();
+      match.ifPresent(available::remove);
+      return match.isPresent();
+    });
+  }
 }
