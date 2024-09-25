3fdec0d3247ab10d52ccfc6fef5c70223663c503
Mrbysco <Mrbysco@users.noreply.github.com>
2024-09-25 17:31:24 +0200

    Return Ingredient.EMPTY (See #107)

--- src/main/java/elucent/rootsclassic/registry/RootsItemTier.java
+++ src/main/java/elucent/rootsclassic/registry/RootsItemTier.java
@@ -1,15 +1,16 @@
 package elucent.rootsclassic.registry;
 
 import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.neoforged.neoforge.common.SimpleTier;
 
 public class RootsItemTier {
 
 	public static final Tier LIVING = new SimpleTier(
 		RootsTags.INCORRECT_FOR_LIVING_TOOL, 192, 6.0f, 2.0f, 18,
-		() -> null);
+		() -> Ingredient.EMPTY);
 
 	public static final Tier ENGRAVED = new SimpleTier(
 		RootsTags.INCORRECT_FOR_ENGRAVED_TOOL, 1050, 5F, 8.0F, 5,
-		() -> null);
+		() -> Ingredient.EMPTY);
 }
