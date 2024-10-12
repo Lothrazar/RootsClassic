7b699c3e41e07b1c72f967535982fc8456debd62
Mrbysco <Mrbysco@users.noreply.github.com>
2024-10-12 14:24:19 +0200

    Add items to more applicable tags (See #114)

--- src/main/java/elucent/rootsclassic/datagen/server/RootsItemTagsProvider.java
+++ src/main/java/elucent/rootsclassic/datagen/server/RootsItemTagsProvider.java
@@ -11,6 +11,7 @@ import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.neoforged.neoforge.common.Tags;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 
 import java.util.concurrent.CompletableFuture;
@@ -32,12 +33,24 @@ public class RootsItemTagsProvider extends ItemTagsProvider {
 			RootsRegistry.WHITECURRANT.get(),
 			RootsRegistry.ELDERBERRY.get()
 		);
+    this.tag(Tags.Items.FOODS_BERRY).addTag(RootsTags.BERRIES);
+    this.tag(Tags.Items.FOODS).add(RootsRegistry.OLD_ROOT.get(), RootsRegistry.HEALING_POULTICE.get(), RootsRegistry.FRUIT_SALAD.get());
+    this.tag(Tags.Items.FOODS_SOUP).add(RootsRegistry.ROOTY_STEW.get());
 		this.addBark(RootsRegistry.ACACIA_BARK.get(), "acacia");
 		this.addBark(RootsRegistry.BIRCH_BARK.get(), "birch");
 		this.addBark(RootsRegistry.DARK_OAK_BARK.get(), "dark_oak");
 		this.addBark(RootsRegistry.JUNGLE_BARK.get(), "jungle");
 		this.addBark(RootsRegistry.OAK_BARK.get(), "oak");
 		this.addBark(RootsRegistry.SPRUCE_BARK.get(), "spruce");
+    this.tag(ItemTags.AXES).add(RootsRegistry.LIVING_AXE.get());
+    this.tag(ItemTags.HOES).add(RootsRegistry.LIVING_HOE.get());
+    this.tag(ItemTags.PICKAXES).add(RootsRegistry.LIVING_PICKAXE.get());
+    this.tag(ItemTags.SHOVELS).add(RootsRegistry.LIVING_SHOVEL.get());
+    this.tag(ItemTags.SWORDS).add(RootsRegistry.LIVING_SWORD.get(), RootsRegistry.ENGRAVED_BLADE.get());
+    this.tag(ItemTags.HEAD_ARMOR).add(RootsRegistry.SYLVAN_HOOD.get(), RootsRegistry.WILDWOOD_MASK.get());
+    this.tag(ItemTags.CHEST_ARMOR).add(RootsRegistry.SYLVAN_ROBE.get(), RootsRegistry.WILDWOOD_PLATE.get());
+    this.tag(ItemTags.LEG_ARMOR).add(RootsRegistry.SYLVAN_ROBE.get(), RootsRegistry.WILDWOOD_LEGGINGS.get());
+    this.tag(ItemTags.FOOT_ARMOR).add(RootsRegistry.SYLVAN_BOOTS.get(), RootsRegistry.WILDWOOD_BOOTS.get());
 	}
 
 	private void addBark(Item item, String treeType) {
