7bdb8c5a7ddf985859475b9492f860cd5524fc13
Mrbysco <Mrbysco@users.noreply.github.com>
2024-12-25 18:13:08 +0100

    Remove unused import

--- src/main/java/elucent/rootsclassic/datagen/server/RootsBlockTagsProvider.java
+++ src/main/java/elucent/rootsclassic/datagen/server/RootsBlockTagsProvider.java
@@ -1,35 +1,13 @@
 package elucent.rootsclassic.datagen.server;
 
 import elucent.rootsclassic.Const;
-import elucent.rootsclassic.block.AcceleratorStandingStoneBlock;
-import elucent.rootsclassic.block.AestheticStandingStoneBlock;
-import elucent.rootsclassic.block.AttunedStandingStoneBlock;
-import elucent.rootsclassic.block.EntanglerStandingStoneBlock;
-import elucent.rootsclassic.block.GrowerStandingStoneBlock;
-import elucent.rootsclassic.block.IgniterStandingStoneBlock;
-import elucent.rootsclassic.block.MundaneStandingStoneBlock;
-import elucent.rootsclassic.block.RepulsorStandingStoneBlock;
-import elucent.rootsclassic.block.VacuumStandingStoneBlock;
-import elucent.rootsclassic.blockentity.AcceleratorStandingStoneTile;
-import elucent.rootsclassic.blockentity.AestheticStandingStoneTile;
-import elucent.rootsclassic.blockentity.EntanglerStandingStoneTile;
-import elucent.rootsclassic.blockentity.GrowerStandingStoneTile;
-import elucent.rootsclassic.blockentity.IgniterStandingStoneTile;
-import elucent.rootsclassic.blockentity.RepulsorStandingStoneTile;
-import elucent.rootsclassic.blockentity.VacuumStandingStoneTile;
 import elucent.rootsclassic.registry.RootsRegistry;
 import elucent.rootsclassic.registry.RootsTags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.tags.BlockTags;
-import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
-import net.minecraft.world.level.material.PushReaction;
 import net.neoforged.neoforge.common.data.BlockTagsProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
-import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredHolder;
 
 import javax.annotation.Nullable;
 import java.util.concurrent.CompletableFuture;
