01208517bb54eb15d4c4502bbaebb9e26f50b817
Mrbysco <Mrbysco@users.noreply.github.com>
2026-04-26 14:25:53 +0200

    Change the shear check for the Global Loot Modifier

--- src/main/java/elucent/rootsclassic/datagen/server/RootsGLMProvider.java
+++ src/main/java/elucent/rootsclassic/datagen/server/RootsGLMProvider.java
@@ -2,16 +2,14 @@ package elucent.rootsclassic.datagen.server;
 
 import elucent.rootsclassic.Const;
 import elucent.rootsclassic.lootmodifiers.DropModifier;
-import net.minecraft.advancements.criterion.ItemPredicate;
 import net.minecraft.core.HolderLookup;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.level.storage.loot.predicates.InvertedLootItemCondition;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
-import net.minecraft.world.level.storage.loot.predicates.MatchTool;
-import net.neoforged.neoforge.common.Tags;
+import net.neoforged.neoforge.common.ItemAbilities;
 import net.neoforged.neoforge.common.data.GlobalLootModifierProvider;
+import net.neoforged.neoforge.common.loot.CanItemPerformAbility;
+import net.neoforged.neoforge.common.loot.IGlobalLootModifier;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -23,11 +21,11 @@ public class RootsGLMProvider extends GlobalLootModifierProvider {
 
 	@Override
 	protected void start() {
-    HolderLookup.RegistryLookup<Item> items = this.registries.lookupOrThrow(Registries.ITEM);
-
 		this.add("rootsclassic_drops", new DropModifier.BlockDropModifier(
 			new LootItemCondition[]{
-				InvertedLootItemCondition.invert(MatchTool.toolMatches(ItemPredicate.Builder.item().of(items, Tags.Items.TOOLS_SHEAR))).build()
-			}, 1000));
+				InvertedLootItemCondition.invert(
+          CanItemPerformAbility.canItemPerformAbility(ItemAbilities.SHEARS_DIG)
+        ).build()
+			}, IGlobalLootModifier.DEFAULT_PRIORITY));
 	}
 }
